4c3552b1f4e2533c53979a6d1b68c475bd64b14a
Farhan Aftab <139067612+farhanaftab1234@users.noreply.github.com>
2024-05-20 09:53:04 +0000

    Variables and datatypes

---
+++ 01basics/variables.js
@@ -0,0 +1,21 @@
+// Variables In Javscript 
+
+const userName = "Farhan";
+console.log(userName);
+
+//const is a keyword for declaring variables in javascript 
+// any variable declared using const is unchangeable //
+// lets look
+
+//userName = "Aftab"; this line is not allowed 
+
+
+ let userId = 9546;
+ userId = 8603;
+ console.log(userId);
+
+
+ // here line 14 will be executed 
+ // coz we changed the value stored in userId and declared this vatriable using let not const keyword
+
+
