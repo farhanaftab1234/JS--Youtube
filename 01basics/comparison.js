369ab24547940e3b3389b462c2fd376a6b49df29
Farhan Aftab <139067612+farhanaftab1234@users.noreply.github.com>
2024-05-23 17:16:33 +0000

    equality nd comparison opeartor

---
+++ 01basics/comparison.js
@@ -0,0 +1,19 @@
+let myobject = [ 1, 2 , 3];
+let x = "1,2,3";
+// comparing two opearnds of same datatypes is easy 
+// things gets confusing when either of the one operands is of different types
+
+// == this operator should be avoided because it only compares the value not the datatype instead of this use triple equal ===
+
+
+console.log( myobject == x);
+console.log("02">1);
+console.log("02"==2);
+console.log(2 >="02");
+
+// Comparing null with zero wuth diff comparison
+
+console.log(null > 1);
+console.log( null >= 0);
+console.log(null == 0);
+// note here comparison operator is converting null to zero and comparing with 0 and equality operator is not converting null to zero
