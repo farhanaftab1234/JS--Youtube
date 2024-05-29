237869b1fe2a88c251fa8bb7364570d384801cfc
Farhan Aftab <139067612+farhanaftab1234@users.noreply.github.com>
2024-05-29 09:00:53 +0000

    more methods

--- 01basics/Strings and Methods.js
+++ 01basics/Strings and Methods.js
@@ -33,6 +33,31 @@ console.log(greatestPlayer.endsWith("century")); // output true cause the string
 console.log(greatestPlayer.endsWith("kohli", 11));
 // this checks the string "kohli" ends on index 11 and returns value in boolean
 
+//method6 
+// includes() => searches the specified string in the string 
 
+const otherStr  = "The man has three pets and one of them is a tigress";
+const pets = otherStr.includes("tigress");
+console.log(`the man has a tigress : ${pets}`);
+// this methods searches the string "tigress in the otherStr variable and returns value in boolean"
+
+//method7 trim()
+
+const longPara = "        The method trim trims the whitespaces of the String from both starting and ending sides.         ";
+// the above strings contains too much unnecessary spaces at start and end to trim this we use the method trim()
+console.log(longPara); // with unnecessary spaces 
+console.log(longPara.trim()); // without unnecessary spaces 
+
+// method8 indexof()
+
+const newTest = " bring your tuesday homework"
+console.log(newTest.indexOf("your"));// output => 7 index of the first occurence of "your";
+
+//method9 padend()
+ const callMewhenFree = "will you call me";
+ console.log(callMewhenFree.padEnd(23," please")); // this will add please to the end of the string till the 23 index 
+ //if the index is more it will repeat
+
+ 
 
 
